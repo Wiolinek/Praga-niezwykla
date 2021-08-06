1e4488566be67b58183608b4113792a888f31ffa
Wiola P <wiol@onet.eu>
2021-08-06 11:13:07 +0200

    images routes fix

--- productsData.js
+++ productsData.js
@@ -5,28 +5,28 @@ export default {
             name: 'Czeska Klasyka, czyli to co najlepsze.',
             description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi eum ullam libero ducimus? Vero, et! Deleniti distinctio omnis, eveniet sunt consequatur, quisquam non iure id culpa reprehenderit quibusdam. Eaque necessitatibus quisquam saepe officiis doloremque placeat ab cumque eveniet magni, culpa nam consequuntur, facilis ipsam laborum dicta assumenda labore commodi doloribus corrupti pariatur!',
             price: 99,
-            photo: '../images/box_1.jpg',
+            photo: 'box_1.jpg',
         },
         {
             id: 2,
             name: 'Kostka Cukru, czyli osłódź sobie życie.',
             description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi eum ullam libero ducimus? Vero, et! Deleniti distinctio omnis, eveniet sunt consequatur, quisquam non iure id culpa reprehenderit quibusdam. Eaque necessitatibus quisquam saepe officiis doloremque placeat ab cumque eveniet magni, culpa nam consequuntur, facilis ipsam laborum dicta assumenda labore commodi doloribus corrupti pariatur!',
             price: 69,
-            photo: '../images/box_2.jpg',
+            photo: 'box_2.jpg',
         },
         {
             id: 3,
             name: 'Bohemia alias Czechia, czyli Czechy to też Kobieta.',
             description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi eum ullam libero ducimus? Vero, et! Deleniti distinctio omnis, eveniet sunt consequatur, quisquam non iure id culpa reprehenderit quibusdam. Eaque necessitatibus quisquam saepe officiis doloremque placeat ab cumque eveniet magni, culpa nam consequuntur, facilis ipsam laborum dicta assumenda labore commodi doloribus corrupti pariatur!',
             price: 39,
-            photo: '../images/box_3.jpg',
+            photo: 'box_3.jpg',
         },
         {
             id: 4,
             name: 'Kde Domov Můj, czyli to co dała czeska ziemia.',
             description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi eum ullam libero ducimus? Vero, et! Deleniti distinctio omnis, eveniet sunt consequatur, quisquam non iure id culpa reprehenderit quibusdam. Eaque necessitatibus quisquam saepe officiis doloremque placeat ab cumque eveniet magni, culpa nam consequuntur, facilis ipsam laborum dicta assumenda labore commodi doloribus corrupti pariatur!',
             price: 79,
-            photo: '../images/box_4.jpg',
+            photo: 'box_4.jpg',
         },
     ]
 }
