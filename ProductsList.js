77ca408a174b59f118ee1c86b09b60aee21951c2
Wiola P <wiol@onet.eu>
2021-08-16 13:07:23 +0200

    routes fix

--- ProductsList.js
+++ ProductsList.js
@@ -24,7 +24,6 @@ const ProductsList = {
                             <h2>${product.name}</h2>
                             <p>${product.description}</p>
                             <div class="boxes__box-price"><p>${product.price} PLN</p>
-                                // <button class="add-to-cart-btn">ZAMÓW</button>
                                 <a class="add-to-cart-btn" href="/Praga-niezwykla/pages/contact.html"></a>
                             </div>
                         </div>
